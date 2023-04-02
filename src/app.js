dcfe36a7f1bc880d9d2d49afc208f41af1e713a7
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-03 08:56:03 +0900

    mongooseによるDB接続を実装

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const taskRoute = require('./routes/tasks')
+const connectDB = require('./mongoose-connect')
 const port = 8080
 
 
@@ -10,6 +11,22 @@ const port = 8080
  * 第２引数にルーターファイルを指定
  */
 app.use("/api/v1/tasks", taskRoute)
+
+/**
+ * DB接続用の関数
+ */
+const url = 'mongodb://test:pass@mongo:27017/';
+const start = async (url) => {
+  try {
+    await connectDB(url);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// データベースと接続開始
+start(url)
 
 //8080番ポートでサーバーを待ちの状態にする。サーバー起動ログを出力する
 app.listen(port, () => console.log(`サーバー起動中 ポート番号：${port}`))
