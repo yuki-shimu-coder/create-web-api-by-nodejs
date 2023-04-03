a1277aa8167c20f79bf0807279d9e012da6979bb
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-04 08:02:34 +0900

    タスクの追加処理を実装

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require('../models/Task')
+
 // 各ルートに対応したメソッドを定義する
 
 // 全タスクの取得
@@ -21,8 +23,17 @@ const deleteTask = (req, res) => {
 }
 
 // タスクの追加
-const createTask = (req, res) => {
-  res.send('タスクを新規作成しました')
+const createTask = async (req, res) => {
+  try {
+    // createの引数はjson形式を指定するため、req.bodyを指定。
+    const createTask = await Task.create(req.body)
+    
+    // res.status(200)は、HTTPレスポンスのステータスコードを200 OKに設定。
+    // .json(createTask)は、createTaskオブジェクトをJSON形式に変換し、HTTPレスポンスの本文として返却。
+    res.status(200).json(createTask) 
+  } catch (error) {
+    res.status(500).json(err)
+  }
 }
 
 module.exports = { getAllTasks, getSingleTask, updateTask, deleteTask, createTask }
