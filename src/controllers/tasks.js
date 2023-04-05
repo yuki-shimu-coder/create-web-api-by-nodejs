8cd8fe18e23e247c5f278469b6e5cfde721ed0e5
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-06 07:41:04 +0900

    特定のタスクの取得処理を実装、リクエストidのバリデーションチェックを実装

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,5 +1,6 @@
 // 各ルートに対応したメソッドを定義するファイル
 const Task = require('../models/Task')
+const mongoose = require('mongoose')
 
 // 全タスクの取得
 const getAllTasks = async (req, res) => {
@@ -7,13 +8,32 @@ const getAllTasks = async (req, res) => {
     const allTask = await Task.find({})
     res.status(200).json(allTask)
   } catch (error) {
-    res.status(500).json(err)
+    res.status(500).json(error)
   }
 }
 
 // 特定のタスクの取得
-const getSingleTask = (req, res) => {
-  res.send('ある特定のタスクを取得しました')
+const getSingleTask = async (req, res) => {
+  try {
+    // リクエストされたidが正しい形式なのかバリデーションチェック
+    if (mongoose.isValidObjectId(req.params.id)){
+      const getSingleTask = await Task.findById(req.params.id).exec()
+
+      // 　リクエストされたidが存在しなければエラーレスポンス
+      if (!getSingleTask) {
+        return res.status(404).json(`_id:${req.params.id}は存在しません`);
+      }
+      res.status(200).json(getSingleTask)
+
+      // 形式が正しくなければエラーレスポンス
+    } else {
+      console.log("形式が正しくありません");
+      res.status(400).json(`リクエストされたid[${req.params.id}]の形式が正しくありません`)
+    }
+  } catch (error) {
+    console.log("error", error)
+    res.status(500).json(error)
+  }
 }
 
 // 特定のタスクの更新
@@ -36,7 +56,7 @@ const createTask = async (req, res) => {
     // .json(createTask)は、createTaskオブジェクトをJSON形式に変換し、HTTPレスポンスの本文として返却。
     res.status(200).json(createTask)
   } catch (error) {
-    res.status(500).json(err)
+    res.status(500).json(error)
   }
 }
 
