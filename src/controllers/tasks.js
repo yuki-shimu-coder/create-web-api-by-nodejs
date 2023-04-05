26fbf0b5380d55889af0c5cc0e52dc04da872550
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-06 08:42:26 +0900

    タスクの更新処理を実装

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -16,7 +16,7 @@ const getAllTasks = async (req, res) => {
 const getSingleTask = async (req, res) => {
   try {
     // リクエストされたidが正しい形式なのかバリデーションチェック
-    if (mongoose.isValidObjectId(req.params.id)){
+    if (mongoose.isValidObjectId(req.params.id)) {
       const getSingleTask = await Task.findById(req.params.id).exec()
 
       // 　リクエストされたidが存在しなければエラーレスポンス
@@ -27,7 +27,8 @@ const getSingleTask = async (req, res) => {
 
       // 形式が正しくなければエラーレスポンス
     } else {
-      console.log("形式が正しくありません");
+      console.log('タスクの取得に失敗');
+      console.log(`リクエストされたid[${req.params.id}]の形式が正しくありません`);
       res.status(400).json(`リクエストされたid[${req.params.id}]の形式が正しくありません`)
     }
   } catch (error) {
@@ -37,8 +38,39 @@ const getSingleTask = async (req, res) => {
 }
 
 // 特定のタスクの更新
-const updateTask = (req, res) => {
-  res.send('ある特定のタスクを更新しました')
+const updateTask = async (req, res) => {
+  // アルゴリズムをここに追加する
+
+  // UPDATEに関連するメソッドがあるか？おそらく引数はIDだと思われる。
+  try {
+    // リクエストされたidが正しい形式なのかバリデーションチェック
+    if (mongoose.isValidObjectId(req.params.id)) {
+      const updateTask = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        // 第3引数でオプションを設定
+        {
+          // 更新が適用された後のドキュメントが返却されるように設定
+          new: true
+        }
+      ).exec()
+
+      // 　リクエストされたidが存在しなければエラーレスポンス
+      if (!updateTask) {
+        return res.status(404).json(`_id:${req.params.id}は存在しません`);
+      }
+      res.status(200).json(updateTask)
+
+      // 形式が正しくなければエラーレスポンス
+    } else {
+      console.log('タスクの更新に失敗');
+      console.log(`リクエストされたid[${req.params.id}]の形式が正しくありません`);
+      res.status(400).json(`リクエストされたid[${req.params.id}]の形式が正しくありません`)
+    }
+  } catch (error) {
+    console.log("error", error)
+    res.status(500).json(error)
+  }
 }
 
 // 特定のタスクを削除
