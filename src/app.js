fb3c0989e719f6e8ffcc0c362683cf4bbb93ad7d
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-11 08:43:51 +0900

    todoの雛形を作成

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const url = 'mongodb://test:pass@mongo:27017/' // DB接続URL
 
 // json形式を利用することを明示する
 app.use(express.json())
+
+// 静的ファイルの利用を宣言
+app.use(express.static('./public'))
+
 /**
  * ルーティング設計
  * 第一引数にエンドポイントの共通部分を指定
