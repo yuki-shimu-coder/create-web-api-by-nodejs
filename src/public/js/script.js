3a01cf2c5030b15e8167d65433f09ced8d06d8c2
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-13 08:20:23 +0900

    タスクの新規追加処理を実装

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -3,15 +3,17 @@
 // タスクを挿入するDOMを取得する
 const tasksDOM = document.getElementById('tasks')
 
+// フォームのDOMを取得する
+const formDOM = document.getElementById('task-form')
+
+// テキストボックスのDOMを取得する
+const inputTask = document.getElementById('task-input')
 
 // /api/v1/tasksから全タスクを取得する
 const showTasks = async () => {
   try {
     // 全タスクの取得
     const tasks = await axios.get('/api/v1/tasks')
-
-    console.log(tasks);
-
     // 取得したタスクをを出力する
     const allTasks = tasks.data.map((task) => {
       const { completed, name, _id } = task
@@ -36,4 +38,26 @@ const showTasks = async () => {
   }
 
 }
+
+// 初回表示時に実行する
 showTasks()
+
+
+// タスクを新規追加する
+// formのsubmitイベントを定義する
+formDOM.addEventListener('submit', async (event) => {
+  // APIを叩ければ良いので、デフォルトの動作をキャンセルする
+  event.preventDefault()
+  // POSTするdataを用意する
+  const data = {
+    // nameは、データスキーマ構造の名前
+    name: inputTask.value
+  }
+  // 入力されたタスクをjson形式でエンドポイントに投げる
+  const createTask = await axios.post('/api/v1/tasks', data)
+
+  // post後テキストボックスの中身を空にする
+  inputTask.value = ''
+  // 追加処理が終了したらshowTaskを実行する
+  showTasks()
+})
