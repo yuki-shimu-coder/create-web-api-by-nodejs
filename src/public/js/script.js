f81417ad6cad70ce41553e3e54a3839605e74e78
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-14 08:00:30 +0900

    タスクの削除処理を追加

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -14,11 +14,18 @@ const showTasks = async () => {
   try {
     // 全タスクの取得
     const tasks = await axios.get('/api/v1/tasks')
+
+    // タスクがないときは'タスクがありません'を表示する
+    if (tasks.data.length < 1) {
+      tasksDOM.innerHTML = 'タスクがありません'
+      return 
+    }
+
     // 取得したタスクをを出力する
     const allTasks = tasks.data.map((task) => {
       const { completed, name, _id } = task
       return `
-      <div class="single-task">
+      <div class="single-task" data-id="${_id}">
         <h5><span><i class="fa-regular fa-circle-check ${completed ? '' : 'none'}"></i></span>${name}</h5>
         <div class="task-links">
           <!-- 編集リンク -->
@@ -33,6 +40,21 @@ const showTasks = async () => {
 
     // 上記で生成したhtmlを挿入する
     tasksDOM.innerHTML = allTasks
+
+    // ここで削除ボタンに対するイベントリスナーを設定する（htmlの挿入後にDOMを取得できる）
+    tasksDOM.querySelectorAll('.delete-btn').forEach(deleteBtn => {
+      deleteBtn.addEventListener('click', async (event) => {
+        // クリックされた要素から見て、クラス名が.single-taskの祖先要素を取得。その祖先要素に設定されているdata-idを取得。
+        const taskId = event.target.closest('.single-task').dataset.id;
+        try {
+          await axios.delete(`/api/v1/tasks/${taskId}`);
+          showTasks();
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    });
+
   } catch (error) {
     console.log(error);
   }
@@ -43,8 +65,7 @@ const showTasks = async () => {
 showTasks()
 
 
-// タスクを新規追加する
-// formのsubmitイベントを定義する
+// タスクを新規追加する(formのsubmitイベント着火時に実行)
 formDOM.addEventListener('submit', async (event) => {
   // APIを叩ければ良いので、デフォルトの動作をキャンセルする
   event.preventDefault()
@@ -53,11 +74,14 @@ formDOM.addEventListener('submit', async (event) => {
     // nameは、データスキーマ構造の名前
     name: inputTask.value
   }
-  // 入力されたタスクをjson形式でエンドポイントに投げる
-  const createTask = await axios.post('/api/v1/tasks', data)
-
-  // post後テキストボックスの中身を空にする
-  inputTask.value = ''
-  // 追加処理が終了したらshowTaskを実行する
-  showTasks()
+  try {
+    // 入力されたタスクをjson形式でエンドポイントに投げる
+    await axios.post('/api/v1/tasks', data)
+    // post後テキストボックスの中身を空にする
+    inputTask.value = ''
+    // 追加処理が終了したらshowTaskを実行する
+    showTasks()
+  } catch (error) {
+    console.log(error);
+  }
 })
